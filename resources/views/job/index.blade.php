37385c10bf88455d8b40284e57d8b07a36a921aa
lewmeta <dev.lewismeta@gmail.com>
2024-11-22 13:45:46 +0300

    feat(filter-jobs): implement job filtering form with Tailwind CSS
    Added a JobFilter component for filtering job listings.
    Integrated Tailwind CSS's Form plugin for modern input styles.
    Implemented reusable text input and select components for flexibility.
    Connected the filter form to the job listing route for dynamic filtering.
    
    Refs: JOB-BOARD-006, #52

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,9 +1,29 @@
 <x-layout>
-    <x-breadcrumps :links="['Jobs' => route('jobs.index'),]" class="mb-4 container" />
+    <x-breadcrumps :links="['Jobs' => route('jobs.index')]" class="mb-4 container" />
+
+    <x-card class="mb-4 text-sm">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+            <div>
+                <div class="mb-1 font-semibold text-sm">
+                    Search
+                </div>
+                <x-text-input name="search" value="" placeholder="Search for any text" />
+            </div>
+            <div>
+                <div class="mb-1 font-semibold text-sm">
+                    Salary
+                </div>
+                <div class="flex items-center space-x-2">
+                    <x-text-input name="min_salary" value="" placeholder="Min " />
+                    <x-text-input name="max_salary" value="" placeholder="Max" />
+                </div>
+            </div>
+            <div>3</div>
+            <div>4</div>
+        </div>
+    </x-card>
     @foreach ($jobs as $job)
         <x-job-card class="mb-2" :$job>
-            <div>
-            </div>
             <x-link-button class="" :href="route('jobs.show', $job)">
                 Show
             </x-link-button>
