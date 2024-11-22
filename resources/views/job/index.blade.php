000943527e7cfeaebbec8a4cde6a0f32efa9cbe2
lewmeta <dev.lewismeta@gmail.com>
2024-11-22 16:41:40 +0300

    feat(job-filter): implement backend logic for filtering experience
    Updated the JobController to query job posts by experience for filter functionality.
    Enhanced the job listing form to handle query parameters using the GET method, ensuring seamless filtering.
    Improved user experience by dynamically reflecting the search state in the form.

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,24 +2,43 @@
     <x-breadcrumps :links="['Jobs' => route('jobs.index')]" class="mb-4 container" />
 
     <x-card class="mb-4 text-sm">
-        <form action="{{route('jobs.index')}}" method="GET">
+        <form action="{{ route('jobs.index') }}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold text-sm">
                         Search
                     </div>
-                    <x-text-input name="search" value="{{request('search')}}" placeholder="Search for any text" />
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" />
                 </div>
                 <div>
                     <div class="mb-1 font-semibold text-sm">
                         Salary
                     </div>
                     <div class="flex items-center space-x-2">
-                        <x-text-input name="min_salary" value="{{request('min_salary')}}" placeholder="Min " />
-                        <x-text-input name="max_salary" value="{{request('max_salary')}}" placeholder="Max" />
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="Min " />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="Max" />
                     </div>
                 </div>
-                <div>3</div>
+                <div>
+                    <div class="mb-1 font-semibold">Experience</div>
+                    <label for="experience" class="flex items-center">
+                        <input type="radio" name="experience" value="" @checked(!request('experience'))>
+                        <span class="ml-2">All</span>
+                    </label>
+                    <label for="experience" class="flex items-center">
+                        <input type="radio" name="experience" @checked('entry' === request('experience')) value="entry">
+                        <span class="ml-2">Entry</span>
+                    </label>
+                    <label for="experience" class="flex items-center">
+                        <input type="radio" name="experience" value="senior" @checked('senior' === request('experience'))>
+                        <span class="ml-2">Senior</span>
+                    </label>
+                    <label for="experience" class="flex items-center">
+                        <input type="radio" name="experience" value="intermidiate" @checked('intermidiate' === request('experience'))>
+                        <span class="ml-2">Intermidiate</span>
+                    </label>
+
+                </div>
                 <div>4</div>
             </div>
             <button class="w-full">Filter</button>
